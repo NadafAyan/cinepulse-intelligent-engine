cf7b1bdb9ecd571a47a81a4dc101394f3f6e5d5b
Ayan Nadaf <nadafayan95@gmail.com>
2026-02-11 21:11:01 +0530

    feat: Imported all the required packages in the main.py

---
+++ ml-engine/main.py
@@ -0,0 +1,4 @@
+from fastapi import FastAPI, HTTPException
+from app.core.engine import get_content_recommendations
+from app.schemas.recommendation import RecommendationResponse
+from typing import List
