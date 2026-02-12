5146581939013831d21af1d18a8e659cba795eb8
Ayan Nadaf <nadafayan95@gmail.com>
2026-02-12 22:06:27 +0530

    feat: Created a Model - RecommendationResponse.

--- ml-engine/app/schemas/recommendation.py
+++ ml-engine/app/schemas/recommendation.py
@@ -4,3 +4,8 @@ from typing import List
 class MovieInput(BaseModel):
     title: str
     description: str  # We will use this for content filtering
+
+class RecommendationResponse(BaseModel):
+    movie_id: int
+    title: str
+    match_score: float  # How good is the recommendation?
